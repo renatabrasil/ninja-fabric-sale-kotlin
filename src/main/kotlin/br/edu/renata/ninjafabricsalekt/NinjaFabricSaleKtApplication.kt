46fef7ada27ac2b6f7bd80b23ee17d9a2284eca3
Renata Brasil <rebra@example.com>
2023-06-23 22:13:20 -0300

    first commit

---
+++ src/main/kotlin/br/edu/renata/ninjafabricsalekt/NinjaFabricSaleKtApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.renata.ninjafabricsalekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NinjaFabricSaleKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<NinjaFabricSaleKtApplication>(*args)
+}
---
+++ src/test/kotlin/br/edu/renata/ninjafabricsalekt/NinjaFabricSaleKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.renata.ninjafabricsalekt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NinjaFabricSaleKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
