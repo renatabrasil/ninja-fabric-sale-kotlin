05737ba6f07f4e1c4a7bd969561952a2acba591c
Renata Brasil <rebra@example.com>
2023-07-13 22:30:45 -0300

    feat: build unit tests

--- src/test/kotlin/br/edu/renata/ninjafabricsalekt/adapters/outbound/persistence/employees/GetEmployeeByIdAdapterTest.kt
+++ src/test/kotlin/br/edu/renata/ninjafabricsalekt/adapters/outbound/persistence/employees/GetEmployeeByIdAdapterTest.kt
@@ -4,10 +4,14 @@ import br.edu.renata.ninjafabricsalekt.adapters.outbound.persistence.entities.Em
 import br.edu.renata.ninjafabricsalekt.adapters.outbound.persistence.repositories.EmployeeRepository
 import org.jeasy.random.EasyRandom
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mockito.*
+import java.lang.IllegalArgumentException
 import java.util.*
+import kotlin.NoSuchElementException
 
 class GetEmployeeByIdAdapterTest {
     private val employeeRepository: EmployeeRepository = mock(EmployeeRepository::class.java)
@@ -27,6 +31,21 @@ class GetEmployeeByIdAdapterTest {
 
         // Then
         assertEquals(expectedResult.get(), response.toEntity())
+        verify(employeeRepository, atLeastOnce()).findById(any())
+
+    }
+
+    @Test
+    fun `Verify getEmployeeById when not found employee`() {
+        // Given
+        val id = UUID.randomUUID().toString()
+
+        `when`(employeeRepository.findById(eq(id))).thenReturn(Optional.empty())
+
+        // When
+        assertThrows<NoSuchElementException>{ getEmployeeByIdAdapter.findById(id) }
+
+        // Then
         verify(employeeRepository, atLeastOnce()).findById(any())
 
     }
