993d6467a627aac63e3ff6d60aa932647032f224
Renata Brasil <rebra@example.com>
2024-10-16 21:56:28 -0300

    chore: create issue template

--- src/main/kotlin/br/edu/renata/ninjafabricsalekt/NinjaFabricSaleKtApplication.kt
+++ src/main/kotlin/br/edu/renata/ninjafabricsalekt/NinjaFabricSaleKtApplication.kt
@@ -1,7 +1,6 @@
 package br.edu.renata.ninjafabricsalekt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
